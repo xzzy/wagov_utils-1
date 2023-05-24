ce22306f9fb7e5aca23c608b3948270142eac341
Jason Moore <jason.moore@dbca.wa.gov.au>
2023-05-24 17:35:45 +0800

    Version Increase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wagov_utils',
-      version='1.02',
+      version='1.03',
       description='DBCA WA GOV utils',
       url='https://github.com/dbca-wa/wagov_utils',
       author='Department of Biodiversity, Conservation and Attractions',

dae5e8d1dd985f6899565482c303c06f03df2642
Jason Moore <jmoore@austwa.com>
2023-05-24 16:22:20 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='wagov_utils',
+      version='1.01',
+      description='DBCA WA GOV utils',
+      url='https://github.com/dbca-wa/wagov_utils',
+      author='Department of Biodiversity, Conservation and Attractions',
+      author_email='asi@dbca.wa.gov.au',
+      license='BSD',
+      packages=['wagov_utils','wagov_utils.components'
+                ],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
